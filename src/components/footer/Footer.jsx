e833d1fe5895567b2a940600228ff184ed73b5bf
Faizal-1112 <faizalahmed.devs@gmail.com>
2025-08-02 18:34:34 +0530

    fixing

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -27,11 +27,11 @@ const Footer = () => {
   return (
     <div className="h-12 w-full flex items-center justify-between">
       <div className="text-sm">© 2025 Faizal. All rights reserved</div>
-      <div className="flex items-center gap-5 cursor-pointer">
+      {/* <div className="flex items-center gap-5 cursor-pointer">
         {links.map((link) => (
           <Image src={link.img} key={link.id} alt={link.name} width={20} height={20} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
